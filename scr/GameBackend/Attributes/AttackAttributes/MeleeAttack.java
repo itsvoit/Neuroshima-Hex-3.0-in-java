7b69730373b10e618f59998e000b833feeefc879
itsvoit <wojciech.skrzypiec2@gmail.com>
2022-10-21 20:17:48 +0200

    Create AttributeAbstract class from which all the Attributes will extend Start work on MeleeAttack (couldn't finish - no Board structure yet)

--- scr/GameBackend/Attributes/AttackAttributes/MeleeAttack.java
+++ scr/GameBackend/Attributes/AttackAttributes/MeleeAttack.java
@@ -1,11 +1,99 @@
 package GameBackend.Attributes.AttackAttributes;
 
-import GameBackend.Attributes.AttributeInterface;
+import GameBackend.Attributes.AttributeAbstract;
+import GameBackend.Direction;
 import GameBackend.Game.Board;
+import GameBackend.Tiles.Interfaces.Tile;
 
-public class MeleeAttack implements AttributeInterface {
+import java.util.ArrayList;
+
+public class MeleeAttack extends AttributeAbstract {
+	// Fields
+	private int[] attackValues;
+
+	// Constructors
+	private MeleeAttack(){
+		attackValues = new int[Direction.DIRECTIONS];
+	}
+
+	/**
+	 *
+	 * @param name Name of the attribute
+	 * @param description Detailed description of the attribute
+	 */
+	public MeleeAttack(String name, String description) {
+		super(name, description);
+	}
+
+	/**
+	 *
+	 * @param name Name of the attribute
+	 * @param description Detailed description of the attribute
+	 * @param owner Tile to which this attribute is attached to
+	 */
+	public MeleeAttack(String name, String description, Tile owner) {
+		super(name, description, owner);
+	}
+
+	/**
+	 *
+	 * @param name Name of the attribute
+	 * @param description Detailed description of the attribute
+	 * @param attackValues Array of ints where index is direction and content is the amount of damage to deal to that direction. If length is different from DIRECTIONS enum constant then default with be used (no damage)
+	 */
+	public MeleeAttack(String name, String description, int[] attackValues){
+		super(name, description);
+		if (attackValues.length == Direction.DIRECTIONS)
+			this.attackValues = attackValues;
+		else
+			this.attackValues = new int[Direction.DIRECTIONS];
+	}
+
+	/**
+	 *
+	 * @param name Name of the attribute
+	 * @param description Detailed description of the attribute
+	 * @param attackValues Array of ints where index is direction and content is the amount of damage to deal to that direction. If length is different from DIRECTIONS enum constant then default with be used (no damage)
+	 * @param owner Tile to which this attribute is attached to
+	 */
+	public MeleeAttack(String name, String description, int[] attackValues, Tile owner){
+		super(name, description, owner);
+		if (attackValues.length == Direction.DIRECTIONS)
+			this.attackValues = attackValues;
+		else
+			this.attackValues = new int[Direction.DIRECTIONS];
+	}
+
+	// Methods
+	public boolean addAttack(Direction direction, int attackValue){
+		if (attackValues[direction.getValue()] == 0) return false;
+		attackValues[direction.getValue()] = attackValue;
+		return true;
+	}
+
+	public boolean removeAttack(Direction direction){
+		if (attackValues[direction.getValue()] == 0) return false;
+		attackValues[direction.getValue()] = 0;
+		return true;
+	}
+
+	/**
+	 *
+	 * @param attackValues Array of ints where index is direction and content is the amount of damage to deal to that direction. Has to have length equal to DIRECTIONS enum constant
+	 * @return true if length is equal to DIRECTION enum constant; false otherwise
+	 */
+	public boolean setAttackValues(int[] attackValues){
+		if (attackValues.length != Direction.DIRECTIONS) return false;
+		this.attackValues = attackValues;
+		return true;
+	}
+
+	// Implemented interface method
 	@Override
 	public void use(Board board) {
-
+		if (owner == null) return;
+		for (int value : attackValues){
+			//todo finish after board structure
+		}
 	}
 }
