ede383f8e03277e87a8d3194f576770671f7c622
itsvoit <wojciech.skrzypiec2@gmail.com>
2022-10-22 14:07:39 +0200

    Add board concept png Change directions to match the board

--- scr/GameBackend/Direction.java
+++ scr/GameBackend/Direction.java
@@ -1,11 +1,11 @@
 package GameBackend;
 
 public enum Direction {
-	NORTH(0),
-	NORTH_EAST(1),
+	NORTH_EAST(0),
+	EAST(1),
 	SOUTH_EAST(2),
-	SOUTH(3),
-	SOUTH_WEST(4),
+	SOUTH_WEST(3),
+	WEST(4),
 	NORTH_WEST(5);
 
 	private int value;
